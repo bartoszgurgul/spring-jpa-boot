935fc6410f620b7edda7d155c75b6e6d08282dc2
bartoszgurgul <bartoszgurgul@gmail.com>
2021-01-19 12:41:04 +0100

    dodanie adnotacji table oraz kolumn dla podania nowych alernatywnych nazw

--- src/main/java/pl/javastart/springjpaboot/model/Book.java
+++ src/main/java/pl/javastart/springjpaboot/model/Book.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
+@Table(name = "books") // taka adnotacja pozwala nam na wrzucenie osobnej nazwy indywidaunej do DB. Ułatwia to prace z tabelami typu t_tds_vdi_t_cards_pl
 public class Book implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -13,6 +14,7 @@ public class Book implements Serializable {
 IDENTITY - klucz generowany jest po stronie bazy danych na podstawie kolumny o automatycznej inkrementacji wartości. W przypadku MySQL kolumna taka posiada własność auto_increment.
 SEQUENCE - klucz generowany jest po stronie bazy danych z wykorzystaniem sekwencji
 TABLE - klucz generowany z pomocą dodatkowej tabeli zarządzaną przez providera JPA. W przypadku Hibernate tabela ta nazywa się domyślnie hibernate_sequence*/
+    @Column(name = "book_id") // nadawanie indywudualnej nazwy na kolumny w celu unikniecia dbilnch nazw typu id_t_vds_column_I_pl
     private Long id;
     private String isbn;
     @Transient
--- src/main/java/pl/javastart/springjpaboot/model/klucz_zlozony_ii/Person.java
+++ src/main/java/pl/javastart/springjpaboot/model/klucz_zlozony_ii/Person.java
@@ -1,29 +1,29 @@
-package pl.javastart.springjpaboot.model.klucz_zlozony_ii;
-
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import java.io.Serializable;
-
-@Entity
-public class Person implements Serializable {
-    @EmbeddedId
-    private PersonEmbeddedKey key;
-
-    private Integer age;
-
-    public PersonEmbeddedKey getKey() {
-        return key;
-    }
-
-    public void setKey(PersonEmbeddedKey key) {
-        this.key = key;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-}
+//package pl.javastart.springjpaboot.model.klucz_zlozony_ii;
+//
+//import javax.persistence.EmbeddedId;
+//import javax.persistence.Entity;
+//import java.io.Serializable;
+//
+//@Entity
+//public class Person implements Serializable {
+//    @EmbeddedId
+//    private PersonEmbeddedKey key;
+//
+//    private Integer age;
+//
+//    public PersonEmbeddedKey getKey() {
+//        return key;
+//    }
+//
+//    public void setKey(PersonEmbeddedKey key) {
+//        this.key = key;
+//    }
+//
+//    public Integer getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(Integer age) {
+//        this.age = age;
+//    }
+//}
